fc08266eabc6d57fef5800dc1f8be9bbb6bda137
elmoustafi-22 <107369052+Elmoustafi-22@users.noreply.github.com>
2025-04-17 17:37:57 +0100

    created some UI functions for the frontend

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import cors from 'cors';
 import booksRoute from './routes/bookRoute.js'
 
 dotenv.config()
-
 const app = express();
+
 app.use(express.json())
+app.use(cors())
+// app.use(cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type']
+// }))
 
 app.get('/', (req, res) => {
     console.log(req)
---
+++ frontend/src/pages/CreateBooks.jsx
@@ -0,0 +1,82 @@
+import React, {useState} from 'react'
+import axios from 'axios'
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner'
+import { useNavigate } from 'react-router-dom';
+
+
+const CreateBooks = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('')
+  const [publishYear, setPublishYear] = useState('')
+  const [loading, setLoading] = useState(false)
+  const navigate = useNavigate()
+
+  const handleSaveBook = () => {
+    const data = {
+      title,
+      author,
+      publishYear,
+    }
+    setLoading(true);
+    axios
+      .post('http://localhost:8000/books', data)
+      .then(() => {
+        setLoading(false);
+        navigate('/')
+      })
+      .catch((error) => {
+        setLoading(false);
+        alert('An error occured. Please check console')
+        console.log(error)
+      })
+  }
+
+  return (
+    <div className='p-4 font-jost'>
+      <BackButton />
+      <h1 className='text-3xl my-4 font-poppins font-bold'>
+        Create Book
+      </h1>
+      {loading ? <Spinner /> : ''}
+      <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
+        <div className='my-4 flex flex-col gap-4'>
+          <label className='text-xl mr-4 text-gray-500'>
+            Title
+            <input 
+            type='text'
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2 w-full'
+          />
+          </label>
+          
+          <label className='text-xl mr-4 text-gray-500'>
+            Author
+            <input 
+            type='text'
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2 w-full'
+          />
+          </label>
+          
+          <label className='text-xl mr-4 text-gray-500'>
+            Published Year
+            <input 
+            type='text '
+            value={publishYear}
+            onChange={(e) => setPublishYear(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2 w-full'
+          />
+          </label>
+
+          <button className='py-2 px-4 cursor-pointer text-gray-800 text-xl bg-sky-300 m-8 rounded ' onClick={handleSaveBook}>Save</button>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default CreateBooks
