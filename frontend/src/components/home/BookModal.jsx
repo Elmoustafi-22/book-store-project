10201a379d0f9a9bc3b3ba0686497c397199b563
elmoustafi-22 <107369052+Elmoustafi-22@users.noreply.github.com>
2025-04-18 10:51:32 +0100

    fixed a bug

--- frontend/src/components/home/BookModal.jsx
+++ frontend/src/components/home/BookModal.jsx
@@ -16,23 +16,26 @@ const BookModal = ({ book, onClose }) => {
           className="absolute right-6 top-6 text-3xl text-red-600 cursor-pointer"
           onClick={onClose}
         />
+        <h2 className="w-fit px-4 py-1 bg-red-300 rounded-lg">
+          {book.publishYear}
+        </h2>
+        <h4 className="my-2 text-gray-500">{book._id}</h4>
+        <div className="flex justify-start items-center gap-x-2">
+          <PiBookOpenTextLight className="text-red-300 text-2xl" />
+          <h2 className="my-1">{book.title}</h2>
+        </div>
+        <div className="flex justify-start items-center gap-x-2">
+          <BiUserCircle className="text-red-300 text-3xl" />
+          <h2 className="my-1">{book.author}</h2>
+        </div>
+        <p className="mt-4">Anything You want to show</p>
+        <p className="my-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis,
+          laudantium eaque. Delectus molestiae, tempore, assumenda in laboriosam
+          nisi molestias dolore quia fugit earum quod amet quis ea, placeat
+          minus? Quaerat?
+        </p>
       </div>
-      <h2 className="w-fit px-4 py-1 bg-red-300 rounded-lg">
-        {book.publishYear}
-      </h2>
-      <h4 className="my-2 text-gray-500">{book._id}</h4>
-      <div className="flex justify-start items-center gap-x-2">
-        <PiBookOpenTextLight className="text-red-300 text-2xl" />
-        <h2 className="my-1">{book.title}</h2>
-      </div>
-      <div className="flex justify-start items-center gap-x-2">
-        <BiUserCircle className="text-red-300 text-3xl" />
-        <h2 className="my-1">{book.author}</h2>
-      </div>
-      <p className="mt-4">Anything You want to show</p>
-      <p className="my-2">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, laudantium eaque. Delectus molestiae, tempore, assumenda in laboriosam nisi molestias dolore quia fugit earum quod amet quis ea, placeat minus? Quaerat?
-      </p>
     </div>
   );
 };
